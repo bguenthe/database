65204647432715fcf1f3bb7def82794b8474621b
claube <bernd.stuebe@gmail.com>
2018-09-30 09:14:01 +0200

    delete costs

--- mqtt/mqtt.sql
+++ mqtt/mqtt.sql
@@ -1,3 +1,5 @@
 select * from mqtt_logger order by logtime desc;
 
 delete from mqtt_logger where id in (480753);
+
+delete from mqtt_logger where topic in ('monthlycosts/costs/new', 'monthlycosts/costs/delete');
