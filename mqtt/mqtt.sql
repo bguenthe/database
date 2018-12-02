8ac51bef7e2efff6a08d479bea15adc9fd09997b
bguenthe <bernd.stuebe@gmail.com>
2018-12-02 11:13:48 +0100

    init

--- mqtt/mqtt.sql
+++ mqtt/mqtt.sql
@@ -1,5 +1,9 @@
-select * from mqtt_logger order by logtime desc;
+select * from mqtt_logger where topic in ('monthlycosts/costs/new', 'monthlycosts/costs/delete') order by logtime desc;
 
-delete from mqtt_logger where id in (480753);
+update mqtt_logger set payload=replace(payload, 'trinken_gehen', 'drinks') where topic in ('monthlycosts/costs/new', 'monthlycosts/costs/delete');
+
+update mqtt_logger set payload=replace(payload, 'essen_gehen', 'rest') where topic in ('monthlycosts/costs/new', 'monthlycosts/costs/delete');
+
+delete from mqtt_logger where id in (481121,481120);
 
 delete from mqtt_logger where topic in ('monthlycosts/costs/new', 'monthlycosts/costs/delete');
