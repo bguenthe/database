ddc1bf341df9c4947963822f7ae40d07ce398fe7
bguenthe <bernd.stuebe@gmail.com>
2026-06-04 17:42:13 +0200

    invested_funds added

--- mqtt/costs_and_income/costs.sql
+++ mqtt/costs_and_income/costs.sql
@@ -2,7 +2,20 @@ select * from mqtt.public.costs_view
 where 1=1
 and comment like '%Kippen%';
 
-select * from mqtt.public.costs_view
+select * from mqtt.public.income_view
 where 1=1
 --and comment like '%Kippen%';
-order by costs_view.recorddatetime desc;
+order by recorddatetime desc;
+
+select * from mqtt_logger where topic = 'expanses/clientcosts';
+
+select * from mqtt_logger where topic = 'expanses/clientincome';
+
+select count(*) from mqtt_logger where topic = 'expanses/clientcosts';
+select count(*) from mqtt_logger_save_2026 where topic = 'expanses/clientcosts';
+
+select count(*) from mqtt_logger where topic = 'expanses/clientincome';
+
+select count(*) from mqtt_logger_save_2026 where topic = 'expanses/clientincome';
+
+delete from mqtt_logger where id = 1187;
