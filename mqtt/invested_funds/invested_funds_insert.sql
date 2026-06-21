794da454afc41e696edd5fe713953e4dba0bee3d
bguenthe <bernd.stuebe@gmail.com>
2026-06-21 11:37:53 +0200

    invested_funds added

--- mqtt/invested_funds/invested_funds_insert.sql
+++ mqtt/invested_funds/invested_funds_insert.sql
@@ -188,3 +188,42 @@ insert into invested_funds (creation_time, name, type, account, amount, interest
 values (to_date('31.05.2026', 'DD.MM.YYYY'), 'Traders Place Anlagekonto', 'Aktien Anlagekonto',
         'DE67 7003 3100 5333 7320 05',
         256.31, null, null);
+
+-- 21.06.2026
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Postbank Giro direkt', 'Girokonto', 'DE95 2001 0020 0305 3612 01',
+        1923.44, null, '');
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Postbank Giro extra plus', 'Girokonto', 'DE63 2307 0224 0446 6439 02',
+        53.51, null, null);
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Postbank Wertpapierdepot', 'Aktien', '203 4664579 00',
+        2568.50, null, null);
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Postbank Wertpapierdepot Anlagekonto', 'Aktien Anlagekonto',
+        'DE09 2307 0224 0466 4579 00',
+        442.10, null, null);
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Otto Genussrecht', 'Otto Genussrecht', '',
+        3251.70, null, null);
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Ayvens Bank', 'Tagesgeld', 'NL18LPLN2801354686',
+        60168.93, 2.8, null);
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Volkswagen Bank', 'Tagesgeld', 'DE26 2702 0000 1531 3669 85',
+        101379.22, 2.9, 'Befristet bis 03.06.2026');
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Traders Place', 'Aktien', 'DE09 2307 0224 0466 4579 00',
+        27242.15, null, null);
+
+insert into invested_funds (creation_time, name, type, account, amount, interest_rate, comment)
+values (to_date('21.06.2026', 'DD.MM.YYYY'), 'Traders Place Anlagekonto', 'Aktien Anlagekonto',
+        'DE67 7003 3100 5333 7320 05',
+        256.31, null, null);
