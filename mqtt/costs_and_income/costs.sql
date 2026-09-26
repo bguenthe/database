46d697a2a09573789ab6bc565c56350ce546f403
bguenthe <bernd.stuebe@gmail.com>
2026-09-26 15:43:15 +0200

    nun auch urlaubskosten

--- mqtt/costs_and_income/costs.sql
+++ mqtt/costs_and_income/costs.sql
@@ -6,9 +6,9 @@ select sum(costs) from mqtt.public.costs_view
 where 1=1
   and lower(comment) like '%kippen%';
 
-/* Urlaubskosten
+/* Urlaubskosten */
 select * from mqtt.public.costs_view
 where 1=1
 and to_date(recordDateTime, 'YYYY-MM-DD') >= to_date('2026-09-08', 'YYYY-MM-DD')
---and to_date(recordDateTime, 'YYYY-MM-DD') <= to_date('2026-09-25', 'YYYY-MM-DD')
+and to_date(recordDateTime, 'YYYY-MM-DD') <= to_date('2026-09-26', 'YYYY-MM-DD')
 order by costs_view.recorddatetime desc;
